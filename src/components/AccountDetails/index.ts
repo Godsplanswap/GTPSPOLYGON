a80f5a37d3ba3251b1075d7801efacf27b442cc2
David Gomadza <welcome@gtps.finance>
2022-09-18 14:13:30 +0100

    c

--- src/components/AccountDetails/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './AccountDetails';
-export { default as CopyHelper } from './CopyHelper';
-export { default as Transaction } from './Transaction';
--- src/components/Swap/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './Swap';
-export { AdvancedSwapDetails } from './AdvancedSwapDetails';
